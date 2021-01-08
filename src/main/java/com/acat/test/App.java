c6c6f3fa2cc6334fbcdff6d68a125e468ca3efba
Pyaephyo2020656 <pyaephyoezaw.2019@gamil.com>
2021-01-08 11:21:07 +0630

    first commit

---
+++ src/main/java/com/acat/test/App.java
@@ -0,0 +1,17 @@
+package com.acat.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Person obj = new Person();
+       obj.setName("Mg Mg");
+       obj.setAge(18);
+       System.out.println(obj.toString());
+       
+    }
+}
---
+++ src/main/java/com/acat/test/Person.java
@@ -0,0 +1,21 @@
+package com.acat.test;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class Person {
+	
+	
+	private String name;
+
+	private int Age;
+	
+	
+	
+
+}
